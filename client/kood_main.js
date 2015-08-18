851d0e470a3776f6672bffcee2728f86ff3103db
Johnghoon Choi <johnghoon@gmail.com>
2015-08-18 10:42:54 +0900

    sidebar
    sidebar

--- client/kood_main.js
+++ client/kood_main.js
@@ -1,5 +1,25 @@
 Template.koodMain.helpers({
-    list : function(){
-        return this.recipeList;
+    list : function() {
+        var i, j, k=0;
+        var check;
+        var temp = new Array();
+        for(i=0; i<sidebarArray.length; i++)
+            if(sidebarArray[i] != "") check = 1;
+        if(check == 1) {
+            for (i = 0; i < sidebarArray.length; i++) {
+                for (j = 0; j < this.recipeList.length; j++) {
+                    if (sidebarArray[i] == this.recipeList[j].division) {
+                        temp[k] = this.recipeList[i];
+                        k++;
+                    }
+                }
+            };
+            check=0;
+            sidebarArray=["",];
+            return temp;
+        }else{
+            return this.recipeList;
+        }
+
     }
 });
--- client/templates/includes/sidebar_menu.js
+++ client/templates/includes/sidebar_menu.js
@@ -1,5 +1,37 @@
+if(Meteor.isClient){
+    sidebarArray = new Array();
+}
+
 Template.sidebar_menu.events({
-    'click button': function () {
-        Router.go('/writeRecipe');
+    'click .chbox_status_recipe':function(evt, tmpl){
+        if(evt.currentTarget.checked == true)
+        {
+            switch(evt.currentTarget.value){
+                case "beef" : sidebarArray[0] = "beef"; break;
+                case "chicken" : sidebarArray[1] = "chicken"; break;
+                case "pork" : sidebarArray[2] = "pork"; break;
+                case "seafood" : sidebarArray[3] = "seafood"; break;
+                case "vegan" : sidebarArray[4] = "vegan"; break;
+                case "dessert" : sidebarArray[5] = "dessert"; break;
+                case "fusion" : sidebarArray[6] = "fusion"; break;
+                case "etc" : sidebarArray[7] = "etc"; break;
+            }
+        }else{
+            switch(evt.currentTarget.value){
+                case "beef" : sidebarArray[0] = ""; break;
+                case "chicken" : sidebarArray[1] = ""; break;
+                case "pork" : sidebarArray[2] = ""; break;
+                case "seafood" : sidebarArray[3] = ""; break;
+                case "vegan" : sidebarArray[4] = ""; break;
+                case "dessert" : sidebarArray[5] = ""; break;
+                case "fusion" : sidebarArray[6] = ""; break;
+                case "etc" : sidebarArray[7] = ""; break;
+            }
+        }
+        alert(sidebarArray);
+    },
+    'click button[name=Confirm]' : function(evt,tmpl){
+        Router.current().render(Template.sidebar_menu).data();
+        Router.current().render(Template.koodMain).data();
     }
 });
