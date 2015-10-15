1851cf79f97b8436e28811dd2e1613c9021a7013
Johnghoon Choi <johnghoon@gmail.com>
2015-10-15 12:45:00 +0900

    유동적인 UI
    유동적인 UI

--- client/kood_main.js
+++ client/kood_main.js
@@ -23,3 +23,6 @@ Template.koodMain.helpers({
 
     }
 });
+
+
+
--- client/templates/recipes/recipe_list_item.js
+++ client/templates/recipes/recipe_list_item.js
@@ -1,5 +1,21 @@
 Template.recipeListItem.events({
-    'click button[name=btn_delete]' : function(evt,tmpl) {
-        Recipes.remove({_id:this._id});
-    }
+  'click button[name=btn_delete]': function (evt, tmpl) {
+    Recipes.remove({_id: this._id});
+  }
 });
+
+// external js: masonry.pkgd.js, imagesloaded.pkgd.js
+
+$(document).ready(function () {
+  // init Masonry after all images have loaded
+  var $grid = $('.grid').imagesLoaded(function () {
+    $grid.masonry({
+      itemSelector: '.grid-item',
+      percentPosition: true,
+      columnWidth: '.grid-sizer'
+    });
+  });
+
+});
+
+
